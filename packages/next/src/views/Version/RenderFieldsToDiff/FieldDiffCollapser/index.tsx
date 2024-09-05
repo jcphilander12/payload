7b820a77f9c5549e13a44a02b098fd599fe762b1
Francisco Lourenço <208149+franciscolourenco@users.noreply.github.com>
2024-09-05 17:18:49 +0200

    feat: show changes indicator when version diff view field is collapsed

--- packages/next/src/views/Version/RenderFieldsToDiff/FieldDiffCollapser/index.tsx
+++ packages/next/src/views/Version/RenderFieldsToDiff/FieldDiffCollapser/index.tsx
@@ -1,5 +1,5 @@
 import { ChevronIcon } from '@payloadcms/ui'
-import React, { useState } from 'react'
+import React, { useMemo, useState } from 'react'
 
 import Label from '../Label/index.js'
 import './index.scss'
@@ -8,12 +8,25 @@ const baseClass = 'field-diff-collapser'
 
 type Props = {
   children: React.ReactNode
+  comparison: any
   initCollapsed?: boolean
   label: React.ReactNode
+  version: any
 }
 
-const FieldDiffCollapser: React.FC<Props> = ({ children, initCollapsed = false, label }) => {
+const FieldDiffCollapser: React.FC<Props> = ({
+  children,
+  comparison,
+  initCollapsed = false,
+  label,
+  version,
+}) => {
   const [isCollapsed, setIsCollapsed] = useState(initCollapsed)
+
+  const hasChanges = useMemo(() => {
+    return JSON.stringify(version) !== JSON.stringify(comparison)
+  }, [version, comparison])
+
   return (
     <div className={baseClass}>
       <Label>
@@ -25,6 +38,12 @@ const FieldDiffCollapser: React.FC<Props> = ({ children, initCollapsed = false, 
           <ChevronIcon direction={isCollapsed ? 'right' : 'down'} />
         </button>
         {label}
+        {isCollapsed && hasChanges && (
+          <span className={`${baseClass}__has-changes-indicator`}>
+            <span className={`${baseClass}__plus-sign`}>+</span>
+            <span className={`${baseClass}__minus-sign`}>–</span>
+          </span>
+        )}
       </Label>
       {!isCollapsed && <div className={`${baseClass}__content`}>{children}</div>}
     </div>
